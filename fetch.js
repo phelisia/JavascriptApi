1ac781a98b743f7fd66891c93c8c3104206aff12
phelisa <phelisajeruto@gmail.com>
2022-08-22 01:04:45 +0300

    fetched api's

--- fetch.js
+++ fetch.js
@@ -1,9 +1,9 @@
 let products=document.getElementById('products');
-let fruits=document.getElementById('fruits');
-let vegetables=document.getElementById('vegetables');
+let fruits=document.getElementById('fruList');
+let vegetables=document.getElementById('vegList');
 
 // products
-const getProductsList =fetch("http://localhost:5000/products");
+const getProductsList = fetch("http://localhost:5000/products");
 getProductsList
 .then((response) => {
     return response.json();
@@ -13,6 +13,44 @@ getProductsList
       let list = document.createElement('li');
      list.innerHTML=`${prodo.name} `
      products.appendChild(list);
+
+    })
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+
+//   fruits
+const getFruitList = fetch("http://localhost:5000/products/fruits");
+getFruitList
+.then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    data.map(fruity => {
+      let list = document.createElement('li');
+     list.innerHTML=`${fruity.name} `
+     fruits.appendChild(list);
+
+    })
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
+
+//   vegetables
+const getVegetablesList = fetch("http://localhost:5000/products/vegetables");
+getVegetablesList
+.then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    data.map(veggie => {
+     let list = document.createElement('li');
+     list.innerHTML=`${veggie.name} `
+     vegetables.appendChild(list);
 
     })
   })
